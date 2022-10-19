6c14fbd46bc34430074cbab683dec529c30dac15
MJ <110538262+MJ-Nanni@users.noreply.github.com>
2022-10-19 17:49:25 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import "./styles.css";
+ 
+let colectivo : string = prompt("Ha llegado el colectivo?"); // condicion para que el loop se repita
+
+while (colectivo == "No") {
+alert("Keep waiting my friend")
+colectivo = prompt("Ha llegado el colectivo?");
+} 
+alert("See you later alligator!");
